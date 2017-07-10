0add02038246ba311680adc44ab60a40ec8f1523
Nick Nissen <nsn@westsoftdevelopment.dk>
2017-07-10 10:22:04 +0200

    Feat: Implement public_path and mix functions from laravel

---
+++ dotnet-mix/DotnetMix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace nnissen
+{
+    public class DotnetMix
+    {
+        public static Dictionary<string, string> manifest;
+        public static string Mix(string path, string manifestDirectory = "")
+        {
+            if (!path.StartsWith("/"))
+            {
+                path = "/" + path;
+            }
+            if (manifestDirectory != string.Empty && !manifestDirectory.StartsWith("/"))
+            {
+                manifestDirectory = "/" + manifestDirectory;
+            }
+            if (File.Exists(PublicPath(manifestDirectory + "hot")))
+            {
+                return string.Format("//localhost:8080{0}", path);
+            }
+
+            if (manifest == null)
+            {
+                var manifestPath = PublicPath(manifestDirectory + "mix-manifest.json");
+                if (!File.Exists(manifestPath))
+                {
+                    throw new FileNotFoundException();
+                }
+
+                manifest = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(@"" + manifestPath));
+            }
+
+            return manifestDirectory + manifest[path];
+        }
+
+        public static string PublicPath(string path)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+
+            string realtivePath = ConfigurationManager.AppSettings["publicPath"];
+            if (realtivePath == null)
+            {
+                throw new ConfigurationMissingException();
+            }
+            string publicPath = AppDomain.CurrentDomain.BaseDirectory + realtivePath;
+
+            if (path == string.Empty)
+            {
+                return publicPath;
+            }
+            return string.Format(@"{0}{1}", publicPath, path);
+        }
+
+    }
+    [Serializable]
+    public class ConfigurationMissingException : ConfigurationErrorsException
+    { }
+}
