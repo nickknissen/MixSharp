6c5261f66344dbc8f6e4575a5a4cef4b5c630549
Nick Nissen <nsn@westsoftdevelopment.dk>
2017-07-10 10:24:03 +0200

    Feat: Add basic unittests

---
+++ dotnet-mix-test/DotnetMixTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using nnissen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+
+namespace nnissen 
+{
+    [TestClass]
+    public class DotnetMixTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ConfigurationMissingException), "A userId of null was inappropriately allowed.")]
+        public void TestPublicPathMissingConfig()
+        {
+            ConfigurationManager.AppSettings["publicPath"] = null;
+            var path = DotnetMix.PublicPath("");
+        }
+
+        [TestMethod]
+        public void TestPublicPath1()
+        {
+            ConfigurationManager.AppSettings["publicPath"] = "/public";
+            var path = DotnetMix.PublicPath("/mix-manifest.json");
+
+            Assert.AreEqual("/public/mix-manifest.json", path);
+        }
+
+        [TestMethod]
+        public void TestPublicPath2()
+        {
+            ConfigurationManager.AppSettings["publicPath"] = "public";
+            var path = DotnetMix.PublicPath("");
+            Assert.AreEqual(AppDomain.CurrentDomain.BaseDirectory + "public", path);
+        }
+
+        [TestMethod]
+        public void TestMix()
+        {
+            ConfigurationManager.AppSettings["publicPath"] = @"\stubs\public-mix-dev";
+
+            var path = DotnetMix.Mix("css/app.css");
+            Assert.AreEqual("/css/app.css", path);
+        }
+    }
+}
